ea100ac54417e181d680eb4963283ba3f08ce990
Min Huasong <44629028+mhuasong@users.noreply.github.com>
2020-03-13 03:43:23 +0800

    Add files via upload

---
+++ lab/loop3/loop3.s
@@ -0,0 +1,29 @@
+/*下面类似于
+	*dest= *src;
+
+For(i=0;i<10;i++)
+	*(++dest)=*(++src);
+*/
+
+.data 
+src: .int 0,1,2,3,4,5,6,7,8,9 
+dst: .int 9,8,7,6,5,4,3,2,1,0 
+.text 
+.globl _start 
+_start: 
+	mov r2, #10 
+	ldr r0, =src 
+	ldr r1, =dst 
+	LDR R3, [R0] 
+	STR R3,[R1]	 
+	  
+loop: 
+	LDR R3, [R0,#4]	! /*带自动索引的前索引寻址: R3<-[R0+4];R0=R0+4 */ 
+	STR R3, [R1,#4]	! 
+	SUBS r2,r2,#1  
+	BNE loop 	    
+ 
+	swi 0x00900001 
+ 
+.end
+
